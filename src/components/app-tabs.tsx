c5b2d5a00102ed77c3274b89d24d2dd41f151f45
MD-Kayesur <rmdkayesur@gmail.com>
2026-06-07 12:32:30 +0600

    Group tabs under src/app/(tabs) directory and update tab routes

--- src/components/app-tabs.tsx
+++ src/components/app-tabs.tsx
@@ -13,17 +13,13 @@ export default function AppTabs() {
       indicatorColor={colors.backgroundElement}
       labelStyle={{ selected: { color: colors.text } }}>
 
-
-
- <NativeTabs.Trigger name="explore">
+      <NativeTabs.Trigger name="(tabs)/explore">
         <NativeTabs.Trigger.Label>Explore</NativeTabs.Trigger.Label>
         <NativeTabs.Trigger.Icon
           src={require('@/assets/images/tabIcons/explore.png')}
           renderingMode="template"
         />
       </NativeTabs.Trigger>
-
-
 
       <NativeTabs.Trigger name="index">
         <NativeTabs.Trigger.Label>Home</NativeTabs.Trigger.Label>
@@ -33,8 +29,7 @@ export default function AppTabs() {
         />
       </NativeTabs.Trigger>
 
-     
-      <NativeTabs.Trigger name="contact">
+      <NativeTabs.Trigger name="(tabs)/contact">
         <NativeTabs.Trigger.Label>Contact</NativeTabs.Trigger.Label>
         <NativeTabs.Trigger.Icon
           src={require('@/assets/images/tabIcons/explore.png')}
