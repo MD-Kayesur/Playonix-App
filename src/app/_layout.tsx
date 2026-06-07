7d18d0d42df6cbd828ab926321baccce8828bbc0
MD-Kayesur <rmdkayesur@gmail.com>
2026-06-07 16:58:30 +0600

    Handle full-screen routing to login screen inside root layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,4 +1,4 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
+import { DarkTheme, DefaultTheme, ThemeProvider, usePathname } from 'expo-router';
 import { useColorScheme } from 'react-native';
 
 import '../global.css';
@@ -6,14 +6,19 @@ import '../global.css';
 import { AnimatedSplashOverlay } from '@/components/animated-icon';
 import AppTabs from '@/components/app-tabs';
 import { MenuProvider } from '@/context/menu-context';
+import LoginScreen from './login';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+  const pathname = usePathname();
+
+  const isLoginPage = pathname === '/login';
+
   return (
     <MenuProvider>
       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
         <AnimatedSplashOverlay />
-        <AppTabs />
+        {isLoginPage ? <LoginScreen /> : <AppTabs />}
       </ThemeProvider>
     </MenuProvider>
   );
